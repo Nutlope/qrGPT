00cf55fa0882134a2339039408001a4fa2750569
nutlope <hassan4709@gmail.com>
2023-09-16 15:43:14 -0700

    v1 homepage

---
+++ components/Brand.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+
+const Brand = () => (
+  <Link href="/">
+    <img src="/blinder.svg" width={120} height={50} alt="Insert Logo here" />
+  </Link>
+);
+export default Brand;
---
+++ components/CTA.tsx
@@ -0,0 +1,30 @@
+import NavLink from './NavLink';
+
+const CTA = () => (
+  <SectionWrapper>
+    <div className="custom-screen">
+      <div className="max-w-2xl mx-auto text-center">
+        <h2 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+          Get started with QrGPT today
+        </h2>
+        <p className="mt-3 text-gray-600">
+          Generate your first AI QR Code today, 100% free.
+        </p>
+        <NavLink
+          href="/get-started"
+          className="mt-4 inline-block font-medium text-sm text-white bg-gray-800 hover:bg-gray-600 active:bg-gray-900"
+        >
+          Generate your AI QR Code
+        </NavLink>
+      </div>
+    </div>
+  </SectionWrapper>
+);
+
+const SectionWrapper = ({ children, ...props }: any) => (
+  <section {...props} className={`py-16 ${props.className || ''}`}>
+    {children}
+  </section>
+);
+
+export default CTA;
