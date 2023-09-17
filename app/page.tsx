99f9a9a2bafdd8ce748b12fc2e69081fd112104d
nutlope <hassan4709@gmail.com>
2023-09-16 17:39:58 -0700

    added gallery to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import CTA from '@/components/CTA';
 import GradientWrapper from '@/components/GradientWrapper';
 import Hero from '@/components/Hero';
-import Testimonials from '@/components/Testimonials';
+// import Testimonials from '@/components/Testimonials';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <GradientWrapper>
-        <Testimonials />
-      </GradientWrapper>
+      <GradientWrapper>{/* <Testimonials /> */}</GradientWrapper>
       <CTA />
     </>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,14 @@
+'use client';
+
+import Image from 'next/image';
 import NavLink from './NavLink';
+
+let heroImages = ['/1.png', '/2.png', '/3.png', '/4.png', '/5.png', '/6.png'];
 
 export default function Hero() {
   return (
     <section>
-      <div className="custom-screen py-28 text-gray-600">
+      <div className="custom-screen pt-28 text-gray-600">
         <div className="space-y-5 max-w-4xl mx-auto text-center">
           <h1 className="text-4xl text-gray-800 font-extrabold mx-auto sm:text-6xl">
             Generate your next AI QR Code in seconds
@@ -27,7 +32,17 @@ export default function Hero() {
               Learn more
             </NavLink>
           </div>
-          {/* TODO: Insert pics here */}
+          <div className="grid grid-cols-3 gap-4 pt-10">
+            {heroImages.map((image) => (
+              <Image
+                alt="image"
+                src={image}
+                width={500}
+                height={500}
+                className="rounded-lg"
+              />
+            ))}
+          </div>
         </div>
       </div>
     </section>
