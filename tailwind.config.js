6ab2fd9d294c02f16a4dc288de5dbf4f5526d41f
Kevin Hou <k@exafunction.com>
2023-09-20 22:15:41 -0700

    Not sure why we had 2 tailwind configs but consolidating

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,6 +70,11 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
   },
   plugins: [
