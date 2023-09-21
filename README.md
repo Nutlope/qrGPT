71aa24bc28c5b1117213c52cedaf5ef74385b6c8
nutlope <hassan4709@gmail.com>
2023-09-20 23:10:09 -0400

    added waitlist page

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 ## Todos v1.5
 
-- [ ] Add Vercel blob to the project, along with Vercel KV based on Steven's project to save prompt + photo generated (although, don't make prompt unique)
-  - [ ] Add a new page for an automatic OG image when a new one gets generated
+- [ ] Add a new page for an automatic OG image when a new one gets generated
 - [ ] Set up cron to run every 2 mins to hit this endpoint
 - [ ] Change ratelimiting back to 10 seconds
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   const navigation = [
     // { title: 'Testimonials', path: '#testimonials' },
-    { title: 'Home Page', path: '/' },
+    { title: 'Homepage', path: '/' },
   ];
 
   const pathname = usePathname();
@@ -101,7 +101,7 @@ const Navbar = () => {
               })}
               <li>
                 <NavLink
-                  href="/start"
+                  href="/waitlist"
                   className="block font-medium text-sm text-white bg-gray-800 hover:bg-gray-600 active:bg-gray-900 md:inline"
                 >
                   Generate your QR Code
