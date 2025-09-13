044751c354150a981bf72eb19b46a75255abc11a
Hassan El Mghari <hassan4709@gmail.com>
2025-09-13 17:34:50 -0400

    wip

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [
       'pbxt.replicate.delivery',
